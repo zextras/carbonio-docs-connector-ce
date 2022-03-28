260444d0494012fbd847e2d289ed63a1c629eb60
Carbonio maintainers <github@zextras.com>
2022-03-28 14:23:58 +0200

    feat: carbonio release

---
+++ boot/src/main/java/com/zextras/carbonio/docs_connector/Boot.java
@@ -0,0 +1,36 @@
+package com.zextras.carbonio.docs_connector;
+
+import com.google.inject.Injector;
+import java.net.InetSocketAddress;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.jboss.resteasy.plugins.guice.GuiceResteasyBootstrapServletContextListener;
+import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
+
+public class Boot {
+
+  private static Injector injector;
+
+
+  public static void main(String[] args) throws Exception {
+    injector = null;//Guice.createInjector(new DocsConnectorModule());
+    injector.getInstance(Boot.class).boot();
+  }
+
+  public void boot() throws Exception {
+    Server server = new Server(InetSocketAddress.createUnresolved("127.78.0.11", 10_000));
+    ServletContextHandler servletHandler = new ServletContextHandler(server, "/");
+    servletHandler.addEventListener(injector.getInstance(
+      GuiceResteasyBootstrapServletContextListener.class)
+    );
+
+    ServletHolder servletHolder = new ServletHolder(HttpServletDispatcher.class);
+
+    servletHandler.addServlet(servletHolder, "/*");
+    server.setHandler(servletHandler);
+
+    server.start();
+    server.join();
+  }
+}
---
+++ core/src/main/java/com/zextras/carbonio/docs_connector/config/DocsConnectorModule.java
@@ -0,0 +1,11 @@
+package com.zextras.carbonio.docs_connector.config;
+
+import org.jboss.resteasy.plugins.guice.ext.RequestScopeModule;
+
+public class DocsConnectorModule extends RequestScopeModule {
+
+  @Override
+  public void configure() {
+
+  }
+}
