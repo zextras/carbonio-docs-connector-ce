d66a162d1f1dd6f0bd295aced69ebab2116ae9ae
Federico Rispo <federico.rispo@zextras.com>
2023-05-02 09:49:01 +0200

    chore(deps+tests): update deps add test environment (#14)
    - Updated dependencies and cleaned up poms
    - Added all the dependencies necessary for tests
    - Added a simple UT for the CacheManager class
    - Added a simple integration test to verify that everything works
    - Updated the Jenkinsfile to:
       - run all tests
       - generate the jacoco report
       - upload the artifact in artifact devel repositories
    
    
    refs: DOCS-182

---
+++ core/src/test/java-ut/com.zextras.carbonio.docs_connector.cache/CacheManagerTest.java
@@ -0,0 +1,36 @@
+package com.zextras.carbonio.docs_connector.cache;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.zextras.carbonio.docs_connector.dal.dao.OpenDocumentToken;
+import java.time.Duration;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CacheManagerTest {
+
+  @Test
+  void givenACacheManagerTheGetTokenCacheShouldReturnAnEmptyCacheWithTheRightAttributes() {
+    // Given & When
+    CacheManager cacheManager = new CacheManager();
+
+    // Then
+    Cache<String, OpenDocumentToken> tokenCache = cacheManager.getTokenCache();
+    Assertions.assertThat(tokenCache).isNotNull();
+    Assertions.assertThat(tokenCache.estimatedSize()).isEqualTo(0);
+    Assertions.assertThat(tokenCache.policy().expireAfterWrite()).isPresent();
+    Assertions.assertThat(tokenCache.policy().expireAfterAccess()).isEmpty();
+    Assertions.assertThat(tokenCache.policy().expireVariably()).isEmpty();
+    Assertions.assertThat(tokenCache.policy().expireAfterWrite().get().getExpiresAfter())
+      .hasMillis(Duration.ofHours(12).toMillis());
+  }
+
+  @Test
+  void givenACacheManagerTheGetTokenDurationInMsShouldReturnTheRightDurationOfTheTokenInTheCache() {
+    // Given && When
+    CacheManager cacheManager = new CacheManager();
+
+    // Then
+    Assertions.assertThat(cacheManager.getTokenDurationInMs())
+      .isEqualTo(Duration.ofHours(12).toMillis());
+  }
+}
