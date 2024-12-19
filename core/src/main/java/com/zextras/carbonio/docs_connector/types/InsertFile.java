91254349be62bd8c6bbd647d95381266c91bf5b7
Federico Rispo <federico.rispo@zextras.com>
2024-12-19 08:46:09 +0100

    chore(license): add missing licenses and update THIRDPARTIES
    - Add missing licenses for java files
    - Add .reuse/dep5 in order to remove the empty *.license files
    - Update THIRDPARTIES
    
    Refs: DOCS-209

--- core/src/main/java/com/zextras/carbonio/docs_connector/types/InsertFile.java
+++ core/src/main/java/com/zextras/carbonio/docs_connector/types/InsertFile.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 package com.zextras.carbonio.docs_connector.types;
 
 import jakarta.validation.constraints.NotNull;
--- core/src/test/java-it/com/zextras/carbonio/docs_connector/apis/GetDocsEditorAttributesApiIT.java
+++ core/src/test/java-it/com/zextras/carbonio/docs_connector/apis/GetDocsEditorAttributesApiIT.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 package com.zextras.carbonio.docs_connector.apis;
 
 import jakarta.ws.rs.HttpMethod;
